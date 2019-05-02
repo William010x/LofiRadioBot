48bed3e295007e005d5c3b14db4c3ee73fcb46d4
William San <william.san@mail.utoronto.ca>
2019-05-01 22:09:43 -0400

    Remove unused commands

--- src/main/java/commands/HelpCommand.java
+++ src/main/java/commands/HelpCommand.java
@@ -15,7 +15,7 @@ public class HelpCommand extends GeneralCommand {
 	@Override
 	protected void execute(CommandEvent event) {
 		EmbedBuilder embed = EmbedGenerator.makeEmbed();
-		embed.addField("__Commands:__", "\n>riseup\n>help\n>commands\n>ping\n>cat\n>dog\n>thani\n>kick\n>user\n", true);
+		embed.addField("__Commands:__", "\n>help\n>commands\n>ping\n>cat\n>dog\n>kick\n>user\n", true);
 		embed.addField("__Music commands:__", "\n>play\n>add\n>skip\n>pause\n>resume\n>stop\n>join\n>leave\n>clear\n>volume\n>mute\n>unmute\n>clear\n>loop\n>queue\n>airhorn\n>bruh\n>bruhminute\n>sans\n", true);
 		embed.setFooter("Check direct messages for detailed information on each command.", null);
 		event.getChannel().sendMessage(embed.build()).queue();
